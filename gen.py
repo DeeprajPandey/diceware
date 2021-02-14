c89a4fed0a23f24af3ed0058460b13be3f818a74
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-02-14 19:59:51 +0530

    Set up generator and add pseudocode

---
+++ gen.py
@@ -0,0 +1,18 @@
+# gen.py
+# Diceware password generator.
+#
+# Deepraj Pandey
+# 14 Feb, 2021
+
+# Take length of password to be generated (len)
+# TODO: set up argparse with flags and help menu
+
+# Do len times
+# Roll dice 5x and generate index code
+
+# Index into wordlist and get word
+
+# Append to pass
+
+# Print passphrase
+# TODO: better way? copy to clipboard automatically?
