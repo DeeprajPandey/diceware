b7df49065dd65469edcd4065e09bf67987e8632c
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2021-02-15 13:59:49 +0530

    Read from wordlist, add words to searchspace and print passphrase

--- gen.py
+++ gen.py
@@ -7,30 +7,40 @@
 from secrets import randbelow
 from collections import OrderedDict
 
-# Take length of password to be generated (len)
+# Take length of password to be generated (phrase_len)
 # TODO: set up argparse with flags and help menu
-len = 5
+phrase_len = 5
 
-# Do len times
-# Roll dice 5x and generate index code
+# Roll dice 5x and generate index code - `phrase_len` times
 # randbelow(n) [0,n), hence +1. We need 5 random numbers joined to
-# create one index, and `len` such indices
+# create one index, and `phrase_len` such indices
 indices = [("".join([str(randbelow(6) + 1) for _ in range(5)]))
-           for _ in range(len)]
+           for _ in range(phrase_len)]
 
 # Ordered Dict of the indices in original random order for storing words
 search_space = OrderedDict((i, '') for i in indices)
 
-print(search_space.keys())
 # Sort for access in one pass of wordlist
 # There is probably a beter way to do this.
 indices.sort()
 
 # Index into wordlist and get word
+filename = "./word.list"
+with open(filename, 'r') as f:
+    for line in f:
+        # if no indices to search for, break
+        if not len(indices):
+            break
 
-# Append to pass
-for id in search_space:
-    print(id)
+        contents = line.split()
+        if contents[0] == indices[0]:
+            # place the word in `search_space`
+            search_space[indices[0]] = contents[1]
+            # indices is sorted, so continuing from next line works
+            indices.pop(0)
+
+# Generate passphrase
+print("-".join([search_space[id] for id in search_space]))
 
 # Print passphrase
 # TODO: better way? copy to clipboard automatically?
